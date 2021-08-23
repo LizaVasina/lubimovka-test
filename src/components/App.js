96ff5111ebf4954e32ee294f0917bb3faec5da3c
Liza Vasina <liz.vasina@gmail.com>
2021-08-23 12:32:07 +0300

    feat: add plays search

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import PlayCard from './PlayCard';
 import arrowRight from '../images/arrowRight.svg';
+import data from '../data.json';
 
 function App() {
+  const inputElement = document.querySelector('.form__input');
+  const [searchValue, setSearchValue] = React.useState(''); // значение, по которому будем искать
+  const [dataArray, setDataArray] = React.useState(data.result);
+  const [searchMessage, setSearchMessage] = React.useState('Поиск');
+  const [playsArray, setPlaysArray] = React.useState([]);
+
+  function handleChange(evt) {
+    setSearchValue(evt.target.value.toLowerCase());
+  }
+
+
+  function getPlays() {
+    const setUpdates = [];
+    dataArray.forEach(play => {
+      if (play.title.toLowerCase().includes(searchValue)) {
+        
+        setUpdates.push(play);
+      }
+    })
+
+    return setUpdates;
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    inputElement.value = '';
+
+    if (searchValue === '') {
+      return;
+    }
+
+    setPlaysArray(getPlays());
+    
+    if (getPlays().length === 0) {
+      setSearchMessage(`По запросу "${searchValue}" мы ничего не нашли`);
+    } else {
+      setSearchMessage(`По запросу "${searchValue}" мы нашли`);
+    }
+    
+    setSearchValue('');
+  }
+
   return (
     <div className="App">
-        <form className="form">
-          <p className="form__result-text">Поиск</p>
+        <form className="form" onSubmit={handleSubmit}>
+          <p className="form__result-text">{searchMessage}</p>
           <div className="form__container">
-            <input className="form__input" placeholder="Введите свой запрос сюда"></input>
+            <input className="form__input" onChange={handleChange} placeholder="Введите свой запрос сюда" required></input>
             <button type="submit" className="form__button">
               <img className="form__arrow" src={arrowRight} alt="Изображение стрелки, направленной вправо, на кнопке"/>
               Искать
@@ -18,24 +61,14 @@ function App() {
 
         <div className="results">
           <div className="results__plays">
-            <PlayCard
-              title="Август 1999 или Никита, любовь и голуби"
-              lastName="Бжожовский"
-              firstName="Теодор"
-              city="Санкт-Петербург"
-              year="2020" />
-            <PlayCard 
-              title="Августина и Степан"
-              lastName="Пастернак"
-              firstName="Ангелина"
-              city="Санкт-Петербург"
-              year="2020"/>
-            <PlayCard 
-              title="По ту сторону августа"
-              lastName="Евдокимов"
-              firstName="Платон"
-              city="Москва"
-              year="2017"/>
+            {playsArray.length !== 0 ? playsArray.map((play) => {
+              return (<PlayCard title={play.title} 
+              key={play._id} 
+              firstName={play.author_firstName}
+              lastName={play.author_lastName}
+              city={play.city}
+              year={play.year}/>)
+            }) : []}
           </div>
           <div className="results__authors">
 
